b90f04122e0eb27543e93b0c01cc9d8e76903e79
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2021-11-26 17:50:02 +0300

    adding router

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express();
 const db = require("./models");
 
 db.sequelize.sync().then(() => {
+  //rounters
+  const postRouter = require("./routes/Posts");
+  app.use("/posts", postRouter);
+
   app.listen(3001, () => {
     console.log("run on 3001");
   });
