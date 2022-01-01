effcf164cc2294a2b6926be7cf4af641533bdd51
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-01-01 13:56:12 +0300

    installing yub

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Link to="/createPost"> Create a Post</Link>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/createPost" element={<CreatePost />} />
         </Routes>
       </Router>
     </div>
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,60 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yub from "yub";
 
 function CreatePost() {
-  return <div></div>;
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const createPost = (data) => {
+    console.log(data);
+  };
+
+  const validationSchema = Yub.object().shape({
+    title: Yub.string().required(),
+    postText: Yub.string().required(),
+    username: Yub.string().required(),
+  });
+
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={createPost}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="title"
+            placeholder="Enter a title for the post"
+          />
+
+          <label>Post: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="postText"
+            placeholder="Enter Post"
+          />
+
+          <label>Username: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="username"
+            placeholder="Enter Username"
+          />
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 
 export default CreatePost;
