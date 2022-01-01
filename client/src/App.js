e2faf92e8e5978d36fa73ca6291bd0b125a15d93
Abdulkareeem Alramlawi <abdulkarim.k.r11@gmail.com>
2022-01-01 11:53:40 +0300

    adding home and create post page

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,18 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
-    });
-  }, []);
-
   return (
     <div className="App">
-      {listOfPosts.map((value) => {
-        return (
-          <div className="post">
-            <div className="title">{value.title}</div>
-            <div className="body">{value.postText}</div>
-            <div className="footer">{value.username}</div>
-          </div>
-        );
-      })}
+      <Router>
+        <Link to="/createPost"> Create a Post</Link>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
